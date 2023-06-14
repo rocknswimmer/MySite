9af814e938acf2279e2c697fd881fab0f0d51325
Andy Simmons <rocknswimmer116@gmail.com>
2023-06-14 14:15:28 -0400

    about rough draft

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+
+
+  return (
+    <div>
+      <h1>Andrew Simmons</h1>
+      <p>I liken coding to cooking. There are a lot of recipes out there and the real skill in cooking comes from trying out the recipes and blending the best parts to make your favorite dishes even better. I have a solid foundation in coding and can take my solutions and make them work for your needs. Look through this menu to see what I can bring to the table.</p>
+    </div>
+  )
+}
+
+export default About;
