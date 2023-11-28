14d79c596a42cd7549747e74f6d5e0e186474c54
Andy Simmons <rocknswimmer116@gmail.com>
2023-11-28 11:56:20 -0500

    project page and route created

---
+++ src/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Projects() {
+
+
+  return (
+    <div>
+      <h1>Sample project title</h1>
+      <p>project description, links, and teaser photo could go here</p>
+    </div>
+  )
+}
+
+export default Projects;
--- src/index.jsx
+++ src/index.jsx
@@ -9,6 +9,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import About from "./About.jsx";
+import Projects from "./Projects.jsx";
 
 import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
       <Route path='/' element={<App />}/>
       <Route path='/about' element={<About />}/>
+      <Route path='/projects' element={<Projects />}/>
     </Routes>
     </div>
     </Router>
