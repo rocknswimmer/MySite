60b41d258dd83e7238188691f380de1af353f542
Andy Simmons <rocknswimmer116@gmail.com>
2023-11-29 12:59:00 -0500

    contact page up and running

---
+++ src/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Contact() {
+
+
+  return (
+    <div>
+      <h1>Andrew Simmons</h1>
+      <p>Email: andy116@vt.edu</p>
+      <p>Phone: 703-309-5326</p>
+    </div>
+  )
+}
+
+export default Contact;
--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import About from "./About.jsx";
 import Projects from "./Projects.jsx";
+import Contact from "./Contact.jsx";
 
 import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Route path='/' element={<App />}/>
       <Route path='/about' element={<About />}/>
       <Route path='/projects' element={<Projects />}/>
+      <Route path='/contact' element={<Contact />}/>
     </Routes>
     </div>
     </Router>
