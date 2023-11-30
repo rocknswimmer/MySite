65416528cfebaa4d9e47a32f21573e30e3497f4f
Andy Simmons <rocknswimmer116@gmail.com>
2023-11-30 13:21:53 -0500

    refining ideas for projects page

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -5,8 +5,11 @@ function Projects() {
 
   return (
     <div>
-      <h1>Sample project title</h1>
-      <p>project description, links, and teaser photo could go here</p>
+      <h1>My Projects</h1>
+      <h2>Solo Projects</h2>
+      {/* store projects in a database to make this a full stack prot? also do api documentation? */}
+      <h2>Team Projects</h2>
+
     </div>
   )
 }
