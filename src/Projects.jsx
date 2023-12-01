1fb3de1c46ca2a56911e97cc7cea1b45b284552f
Andy Simmons <rocknswimmer116@gmail.com>
2023-12-01 12:16:52 -0500

    projects will show full stack no need for db

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -7,7 +7,7 @@ function Projects() {
     <div>
       <h1>My Projects</h1>
       <h2>Solo Projects</h2>
-      {/* store projects in a database to make this a full stack prot? also do api documentation? */}
+
       <h2>Team Projects</h2>
 
     </div>
