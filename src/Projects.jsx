3283489ffbbf89a050e32da73930877c716dbd2d
Andy Simmons <rocknswimmer116@gmail.com>
2023-12-13 10:44:31 -0500

    project placement

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -7,8 +7,13 @@ function Projects() {
     <div>
       <h1>My Projects</h1>
       <h2>Solo Projects</h2>
+      {/* job site
+      virtual relay
+      modals and accordians educational? */}
 
       <h2>Team Projects</h2>
+      {/* ecommerce site
+      birder App */}
 
     </div>
   )
