5d6f4a5c7abae21038fcb24e548d5cdab34191ad
Andy Simmons <rocknswimmer116@gmail.com>
2024-01-08 11:54:25 -0500

    added accordian for extra details

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Accordion from "./accordian.js"
 
 function ProjectCard({project}) {
 
@@ -8,6 +9,7 @@ function ProjectCard({project}) {
       <h3>{project.title}</h3>
       <div>Photo here with link below</div>
       <p>{project.description}</p>
+      <Accordion title={"More Project Details"} content={project.details} />
 
 
     </div>
--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,6 +1,8 @@
 const projectData = [
-  {title: "sample", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", type: "solo"},
-  {title: "sample2", photo: "url2/permanatelocation2", link: "deployedurl2", description: "summary of project2", type: "team"},
+  {title: "sample", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "solo"},
+  {title: "sample2", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "team"},
+  {title: "sample3", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "solo"},
+  {title: "sample4", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "team"},
 ]
 
 export default projectData;
