7093cb1e8aa93de4db3dd9f60515d636d23f41df
Andy Simmons <rocknswimmer116@gmail.com>
2024-01-15 12:31:45 -0500

    short summarys done

--- src/ProjectData.js
+++ src/ProjectData.js
@@ -1,9 +1,9 @@
 const projectData = [
-  {title: "Virtual Relay", photo: "url/permanatelocation", link: "deployedurl", description: "App used by runners for a Virtual Relay", details: "", type: "solo"},
-  {title: "Ecommerce Front and Back End Updates", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "team"},
-  {title: "Generic Job Board", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "solo"},
-  {title: "Birder Connect", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "team"},
-  {title: "Modals and Accordians", photo: "url/permanatelocation", link: "deployedurl", description: "summary of project", details: "longer description for accordian", type: "edu"},
+  {title: "Virtual Relay", photo: "url/permanatelocation", link: "deployedurl", description: "App used for a Virtual Relay", details: "", type: "solo"},
+  {title: "Ecommerce Front and Back End Updates", photo: "url/permanatelocation", link: "deployedurl", description: "Simulated updates to an Ecommerce sites front end and the back end/API", details: "longer description for accordian", type: "team"},
+  {title: "Generic Job Board", photo: "url/permanatelocation", link: "deployedurl", description: "Build a Job Board that had features missing form the ones I have been using while applying", details: "longer description for accordian", type: "solo"},
+  {title: "Birder Connect", photo: "url/permanatelocation", link: "deployedurl", description: "Simulate a small team build for a client", details: "longer description for accordian", type: "team"},
+  {title: "Modals and Accordians", photo: "url/permanatelocation", link: "deployedurl", description: "Provide a plug and play resource that explains what modals and accordians are", details: "longer description for accordian", type: "edu"},
 ]
 
 export default projectData;
