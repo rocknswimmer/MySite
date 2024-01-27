c1c62f950bf425b161af5f68a5a3b1f8e530caac
Andy Simmons <rocknswimmer116@gmail.com>
2024-01-27 13:39:30 -0500

    basic navigation/setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./About.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
 
   return (
     <div className="binder">
       <div className="buttons">
-        <button>about</button>
-        <button>projects</button>
-        <button>contact</button>
+        <button onClick={() => {navigate("/about")}}>about</button>
+        <button onClick={() => {navigate("/projects")}}>projects</button>
+        <button onClick={() => {navigate("/contact")}}>contact</button>
       </div>
       <div className="pages">
       <Routes>
