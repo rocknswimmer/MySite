741fdb99a1aebae3db4860b007a0ee223f9575da
Andy Simmons <rocknswimmer116@gmail.com>
2024-02-07 14:10:48 -0500

    split top projects to make it look more like a menu

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,27 +8,35 @@ function Projects() {
   return (
     <div className="projects">
       <h1>My Projects</h1>
-      <h2>Solo Projects</h2>
-      {projectData.map((project, i) => {
-        if(project.type === "solo"){
-          return(<ProjectCard project={project} key={i}/>)
-        }
+      <div className="horizontal">
+        <div className="solo">
+          <h2>Solo Projects</h2>
+          {projectData.map((project, i) => {
+            if (project.type === "solo") {
+              return (<ProjectCard project={project} key={i} />)
+            }
 
-      })}
-      {/* Map to Project cards for:
+          })}
+          {/* Map to Project cards for:
       job site
       virtual relay
       modals and accordians educational? */}
+        </div>
 
-      <h2>Team Projects</h2>
-      {/* ecommerce site
+        <div className="team">
+
+          <h2>Team Projects</h2>
+          {/* ecommerce site
       birder App */}
-      {projectData.map((project, i) => {
-        if(project.type === "team"){
-          return(<ProjectCard project={project} key={i}/>)
-        }
+          {projectData.map((project, i) => {
+            if (project.type === "team") {
+              return (<ProjectCard project={project} key={i} />)
+            }
 
-      })}
+          })}
+        </div>
+
+      </div>
 
       <h2>Educational Projects</h2>
       {/* ecommerce site
