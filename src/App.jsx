67f52bdf303fd91dee4def824ac268bb96a379df
Andy Simmons <rocknswimmer116@gmail.com>
2024-02-27 12:42:42 -0500

    basic experience page set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {  BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./About.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
+import Experience from "./Experience.js";
 import { useNavigate } from "react-router-dom";
 
 function App() {
@@ -12,14 +13,16 @@ function App() {
     <div className="binder">
       <div className="paper">
         <div className="buttons">
-          <button onClick={() => { navigate("/about") }} className="abutton">about</button>
-          <button onClick={() => { navigate("/projects") }} className="pbutton">projects</button>
-          <button onClick={() => { navigate("/contact") }} className="cbutton">contact</button>
+          <button onClick={() => { navigate("/about") }} className="abutton">About</button>
+          <button onClick={() => { navigate("/projects") }} className="pbutton">Projects</button>
+          <button onClick={() => { navigate("/experience") }} className="ebutton">Experience</button>
+          <button onClick={() => { navigate("/contact") }} className="cbutton">Contact</button>
         </div>
         <div className="pages">
           <Routes>
             <Route path='/about' element={<About />} />
             <Route path='/projects' element={<Projects />} />
+            <Route path='/experience' element={<Experience />} />
             <Route path='/contact' element={<Contact />} />
           </Routes>
         </div>
---
+++ src/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Experience() {
+
+
+  return (
+    <div className="about">
+      <h1>Experience title?</h1>
+
+    </div>
+  )
+}
+
+export default Experience;
