489c7912bcd171c6c80395202c81445518c24c75
Andy Simmons <rocknswimmer116@gmail.com>
2024-03-12 13:28:46 -0400

    map works

--- src/ExperienceData.js
+++ src/ExperienceData.js
@@ -1,5 +1,7 @@
 const experienceData = [
-  {title: "Virtual Relay",},
+  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
+  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
+  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
 
 ]
 
