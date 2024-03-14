77a349a1600150a710e4b194eb294b08545f3377
Andy Simmons <rocknswimmer116@gmail.com>
2024-03-14 15:36:48 -0400

    temp

--- src/ExperienceData.js
+++ src/ExperienceData.js
@@ -1,5 +1,5 @@
 const experienceData = [
-  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
+  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "should do this in word like last time, rough day, had to fight for low paying matience application because im too qualified. first time fighting for being too good"},
   {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
   {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
 
