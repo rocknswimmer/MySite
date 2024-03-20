edbc666ed6ff49262256700b56f5495828ce4a59
Andy Simmons <rocknswimmer116@gmail.com>
2024-03-20 12:23:17 -0400

    organizing the past

--- src/ExperienceData.js
+++ src/ExperienceData.js
@@ -1,7 +1,17 @@
 const experienceData = [
-  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "should do this in word like last time, rough day, had to fight for low paying matience application because im too qualified. first time fighting for being too good"},
-  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
-  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "testing now, reliving the past when I have the capacity", type: "may want various types of exp in the future"},
+  {title: "Stock Lead", company: "American Eagle", exp: "3 years", skills: "Leadership, Customer Service, Inventory Management", details: "should do this in word like last time, rough day, had to fight for low paying matience application because im too qualified. first time fighting for being too good", type: "paid"},
+
+  {title: "Independent Contractor", company: "Multiple gig platforms", exp: "6 years", skills: "Customer Service, Logistics", details: "should do this in word like last time, rough day, had to fight for low paying matience application because im too qualified. first time fighting for being too good", type: "paid"},
+
+  {title: "Delivery Driver", company: "Marcos Pizza", exp: "1 years", skills: "Customer Service, Food Prep, Logistics", details: "should do this in word like last time, rough day, had to fight for low paying matience application because im too qualified. first time fighting for being too good", type: "paid"},
+
+
+
+  {title: "Full Stack Engineer and Project Manager", company: "Waileys", exp: "2 summers", skills: "Teaching", details: "testing now, reliving the past when I have the capacity", type: "volunteer"},
+
+  {title: "Math Tutor", company: "4th grade math", exp: "2 summers", skills: "Teaching", details: "testing now, reliving the past when I have the capacity", type: "volunteer"},
+
+  {title: "Overhead Light Rewiring", company: "Church", exp: "2 summers", skills: "Teaching", details: "testing now, reliving the past when I have the capacity", type: "volunteer"},
 
 ]
 
