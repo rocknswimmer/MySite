6670083d3c08c1a5f9db1d11f2a34c26ecd3a9d0
Andy Simmons <rocknswimmer116@gmail.com>
2024-03-21 13:56:23 -0400

    server and dependensies set up

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const app = express();
 const compression = require('compression')
 const path = require('path');
 
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD // this was a hard typo to find in the terminal
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -12,6 +22,17 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/views', function(req,res) {
+  res.send("getting around /* get route")
+
+})
+
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+final get route since it will override all others, kinda like css rules
+!!!!!!!!!!!!!
+
+*/
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'), function(err) {
     if (err) {
