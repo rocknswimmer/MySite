672c6cb31fd1f243cba8ee5e81bff7b866c23292
Andy Simmons <rocknswimmer116@gmail.com>
2024-04-10 13:41:51 -0400

    links mapped and working

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -9,6 +9,14 @@ function ProjectCard({project}) {
       <h3>{project.title}</h3>
       <div>Photo here with link below</div>
       <p>{project.description}</p>
+      <h4>{project.link.length > 1 ? "links:" : "Link:"}</h4>
+      <div>
+        {project.link.map((tuple) => {
+
+
+          return (<p>{<a href={tuple[1]}>{tuple[0]}</a>}</p>)
+        })}
+      </div>
       <Accordion title={"More Project Details"} content={<p className="newlines" >{project.details}</p>} />
 
 
