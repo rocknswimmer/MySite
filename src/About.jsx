34cf1d193b0c942a8f1bdb191e1fb3f5fc7ad3df
Andy Simmons <rocknswimmer116@gmail.com>
2024-04-11 14:49:55 -0400

    my mug on about page

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
   return (
     <div className="about">
       <h1>Andrew Simmons</h1>
-      <p>photo here</p>
+      <img  crossorigin="anonymous" src="https://drive.lienuc.com/uc?id=1zmUWCsrvo3L684ZPPdT3tbaHvR4jK2ZA"/>
       <h3>Full Stack Engineer</h3>
       <p className="summary">I liken coding to cooking. There are a lot of recipes out there and the real skill in cooking comes from trying out the recipes and blending the best parts to make your favorite dishes even better. I have a solid foundation in coding and can take my solutions and make them work for your needs. Look through this menu to see what I can bring to the table.</p>
 
