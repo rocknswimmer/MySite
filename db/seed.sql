c759ec4c7037e466f32f9da5ba7af00b3929ce01
Andy Simmons <rocknswimmer116@gmail.com>
2024-09-03 16:35:11 -0400

    seed file preped for update

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,15 @@
 DROP TABLE IF EXISTS views;
+DROP TABLE IF EXISTS clicked;
 
 CREATE TABLE views (
   id SERIAL PRIMARY KEY,
   view_count INTEGER NOT NULL DEFAULT NULL
+);
+
+CREATE TABLE clicked (
+  viewer INTEGER NOT NULL DEFAULT NULL,
+  view_type VARCHAR(13) NOT NULL DEFAULT NULL,
+  view_location VARCHAR(100) NOT NULL DEFAULT NULL
 );
 
 insert into views (view_count) values (0) returning *;
